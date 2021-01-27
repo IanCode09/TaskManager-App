942d6221535873b1d38ee7be06cc4f29b703c2d6
IanCode09 <ianlombu@protonmail.com>
2021-01-27 22:39:04 +0700

    The User/Task Relationship

--- src/index.js
+++ src/index.js
@@ -16,3 +16,15 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server running on port', port);
 })
+
+const main = async () => {
+    const task = await Task.findById('601187da58612c004464d05c')
+    await task.populate('owner').execPopulate()
+    console.log(task.owner)
+
+    // const user = await User.findById('601180dcf862f40f8ca5a1de')
+    // await user.populate('tasks').execPopulate()
+    // console.log(user.tasks);
+}
+
+main()
