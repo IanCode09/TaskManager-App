98fd3123f3d27e6037ab1492c1c1defcbd192d88
IanCode09 <ianlombu@protonmail.com>
2021-01-27 13:35:28 +0700

    BcryptJs

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,5 +3,6 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://ian123:ian123@iancode09.va6uv.mongodb.net/task-manager?retryWrites=true&w=majority', {
         useNewUrlParser: true,
         useCreateIndex: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
+        useFindAndModify: true
 })
