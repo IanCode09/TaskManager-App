dedf68236d0bc895ce0235d935b662a8675a9d6d
IanCode09 <ianlombu@protonmail.com>
2021-01-27 10:43:02 +0700

    Create Endpoint Part II

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,27 +5,3 @@ mongoose.connect('mongodb+srv://ian123:ian123@iancode09.va6uv.mongodb.net/task-m
         useCreateIndex: true,
         useUnifiedTopology: true
 })
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        trim: true,
-        required: true
-    },
-
-    completed: {
-        type: Boolean,
-        default: false
-    }
-})
-
-// const task1 = new Task({
-//     description: "   This is Task2 ",
-//     completed: false
-// })
-
-// task1.save().then(() => {
-//     console.log(task1);
-// }).catch((error) => {
-//     console.log(error);
-// })
